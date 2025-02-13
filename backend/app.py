00b971d008a2006fb372a0dbe5df55750bc026c6
Chananel Baharir <baharirchananel@gmail.com>
2025-02-13 20:45:46 +0200

    first commit /flask

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import os
+import json
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "server is run "
+SAVE_FOLDER = os.path.join(os.getcwd(), 'data')
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    data = request.json
+
+    folder_name = iter(next(data))
+    folder_path = os.path.join(SAVE_FOLDER,folder_name)
+
+    if os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    file_name = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M") + ".json"
+    file_path = os.path.join(SAVE_FOLDER,file_name)
+
+    with open(file_path,"w") as json_file:
+        json.dump(data,json_file,indent=4)
+
+    return jsonify({"message": "Data saved", "file": file_name, "folder": folder_name}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
